9eb5f8113bf071a4b990aa8e3f9ab9b6556f9563
rm13009 <48097647+rm13009@users.noreply.github.com>
2019-04-01 22:38:55 -0600

    Agregada la clase Estudiante
    ...

--- Corto/src/corto/Estudiante.java
+++ Corto/src/corto/Estudiante.java
@@ -5,10 +5,72 @@
  */
 package corto;
 
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author henry
+ * @author Clara Rojas Martìnez
  */
 public class Estudiante {
+    //parametros nombre, apellido, grado y carnet
+    
+    private String nombre="";
+    private String apellido="";
+    private String grado="";
+    private String carnet="";
+    
+    public Estudiante (){
+    }        
+
+    public String obtenerNombre() {
+       return nombre;
+        
+    }
+
+    public void asignarNombre(String nombre) {
+        if (nombre.isEmpty()) {
+            JOptionPane.showMessageDialog(null, " Nombre esta vacio");
+        } else{  
+        this.nombre = nombre;
+        }
+    }
+
+    public String obtenerApellido() {
+         return apellido;
+        
+    }
+
+    public void asignarApellido(String apellido) {
+        if (apellido.isEmpty()) {
+            JOptionPane.showMessageDialog(null, " Apellido esta vacio");
+        } else{  
+        this.apellido = apellido;
+        }
+    }
+
+    public String obtenerGrado() {
+        return grado;
+    
+    }
+    public void asignarGrado(String grado){
+        if (grado.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Grado esta vacio");
+        } else{  
+        this.grado = grado;
+        }
+    }
+
+    public String obtenerCarnet() {
+        return carnet;
+    }
+    
+    public void asignarCarnet(String carnet) {
+        if (carnet.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Carnet esta vacio");
+        } else{  
+        this.carnet = carnet;
+        }
+    }
+    
     
 }
